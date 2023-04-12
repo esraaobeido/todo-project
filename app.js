6d389c8a22f02f3495542ad669740c83a1eeb92c
esraaobeido <esraaabeido123@gmail.com>
2023-04-12 09:02:59 +0000

    update

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-   
-    if(confirm("Do you want to show welcoming message?")){
-    let userAnswer1 = prompt("what is your name?");
-    console.log(userAnswer1);
-    let userAnswer2 = prompt("what is your gender?");
-    if(userAnswer2 == "male"|| userAnswer2 == "Male" ) {
-        userAnswer2 = "Mr";
-    }else if(userAnswer2 == "female" || userAnswer2 == "Female") {
-        userAnswer2 = "Mrs";
-    }else {
-        userAnswer2 ="";
+    'use strict';
+
+    let userAnswer1 = prompt("What is your name?");
+    let userAnswer2 = prompt("What is your gender?");
+
+    if(userAnswer2 != "male" && userAnswer2 != "female"){
+        userAnswer2 = prompt("please enter male or female!");
     }
-    console.log(userAnswer2)
-    let userAnswer3 = prompt("how old are you?");
-    if(userAnswer3<=0){
+     
+    let nikeName ="";
+    if(userAnswer2 == 'male'){
+        nikeName = "Mr";
+    } else if(userAnswer2 == 'female'){
+        nikeName = "Mrs";
+    }
+
+    let userAnswer3 = prompt("How old are you?") ;
+    if(userAnswer3 <= 0){
         alert("please enter you age again");
         userAnswer3 = prompt("your age should be greater than zero");
     }
-    let welcomingMessage = alert("hello " + userAnswer2 + " " + userAnswer1 + " weclome to ToDo site");
-        console.log(welcomingMessage);
-   } 
+
+    let welcomingMessage = (confirm("Do you want to show welcoming message?"))
+    console.log(welcomingMessage);
+    
+    if(welcomingMessage === true){
+        alert("hello " + nikeName + " " + userAnswer1 + " Weclome to ToDo site!");
+    }
     
 
 
