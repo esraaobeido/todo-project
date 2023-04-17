f10f480a8e4959b1f83ec6b7d52f5edeb9821996
esraaobeido <esraaabeido123@gmail.com>
2023-04-17 13:32:38 +0000

    update

--- app.js
+++ app.js
@@ -13,58 +13,57 @@
     //console.log(userAnswer2)
    // let userAnswer3 = prompt("how old are you?");
    // if(userAnswer3<=0){
-     //   alert("please enter you age again");
-     //   userAnswer3 = prompt("your age should be greater than zero");
-   // }
- //   let welcomingMessage = alert("hello " + userAnswer2 + " " + userAnswer1 + " weclome to ToDo site");
-     //   console.log(welcomingMessage);
-  // } 
-let questionOne;
-let questionTwo;
-let questionThree;
+    //   alert("please enter you age again");
+    //   userAnswer3 = prompt("your age should be greater than zero");
+    // }
+    // let welcomingMessage = alert("hello " + userAnswer2 + " " + userAnswer1 + " weclome to ToDo site");
+    //   console.log(welcomingMessage);
+    // } 
 
-  let Q1 = 'Do you like the idea of ​​our website?[enter Yes or No]';
-  let Q2 = 'Do you want to solve the challengs?[enter Yes or No]';
-  let Q3 = 'Would you like to add more than one task per day?[enter Yes or No]';
-  
-  function askQuestion(){
-    questionOne = prompt(Q1);
-    questionTwo = prompt(Q2);
-    questionThree = prompt(Q3);
-    
-    return questionOne, questionTwo, questionThree;
+    let arr = [];
 
-  }
-    
-   function testEmptyAnswer(A1,A2,A3){
-    if(A1 == "") {
-        questionOne = "invalid";
-    }
-    if(A2 == "") {
-        questionTwo = "invalid";
-    }
-    if(A3 == "") {
-        questionThree = "invalid";
-    }
-
-    return questionOne, questionTwo, questionThree;
+function questions(qustion){
+    let answer = prompt(qustion);
+    return answer;
 }
 
-function putAnswersInArray(A1, A2, A3){
-   let arrAnswers = [A1, A2, A3];
-    return arrAnswers;
+function putAnswersInArray(answer){
+    arr.push(answer);
+
 }
 
-function printTheAnswers(A1, A2, A3){
-    let arrAnswers = [A1, A2, A3];
-    return console.log(arrAnswers);
+let userAnswer = "";
+userAnswer = questions('Do you like the idea of ​​our website?[enter Yes or No]');
+putAnswersInArray (userAnswer);
+userAnswer = questions('Do you want to solve the challengs?[enter Yes or No]');
+putAnswersInArray (userAnswer);
+userAnswer = questions('Would you like to add more than one task per day?[enter Yes or No]');
+putAnswersInArray (userAnswer);
+console.log(arr);
+
+function testEmptyAnswer(answers){
+    let validArr = [];
+    for (let i = 0; i < answers.length ; i++){
+        if (answers[i] =="yes"){
+            validArr.push("Yes");
+        }
+        else if (answers[i] =="no"){
+            validArr.push("No");
+        }
+        else{
+            validArr.push("invalid");
+        }
+        
+    }
+
+    return validArr;
+
 }
 
-askQuestion(questionOne, questionTwo, questionThree);
+function printTheAnswers(validAnswers){
+for (let i = 0 ; i < validAnswers.length; i++){
+    console.log (validAnswers[i]);
+}
+}
 
-testEmptyAnswer(questionOne, questionTwo, questionThree);
-
-putAnswersInArray(questionOne, questionTwo, questionThree);
-
-printTheAnswers(questionOne, questionTwo, questionThree);
-
+printTheAnswers(testEmptyAnswer(arr));
